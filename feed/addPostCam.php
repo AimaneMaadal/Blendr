4ced3c08199c95571f6c989429510b1864aad237
AimaneMaadal <aimanemaadal@gmail.com>
2022-05-16 02:21:54 +0200

    feat: camera style

--- feed/addPostCam.php
+++ feed/addPostCam.php
@@ -13,42 +13,69 @@ session_start();
     <link rel="stylesheet" href="feed.css">
     <link rel="stylesheet" href="../style.css">
 	
-
-</head>
-<body onload="configure()">
-
-	<!-- CSS -->
     <style>
     #my_camera{
         width: 320px;
         height: 240px;
-        border: 1px solid black;
 		border-radius: 10px;
     }
 	#results{
         width: 320px;
         height: 240px;
-        border: 1px solid black;
 		margin-top: 150px;
 		border-radius: 10px;
     }
 	#my_camera video{
 		border-radius: 10px;
     }
+	.fileChoice{
+		display: flex;
+		justify-content: space-around;
+		margin-top: 162px;
+		width: 80%;
+	}
+	.fileChoice a:first-child{
+		font-weight: 600px;
+	}
+	.activeChoice{
+		border-bottom: 3px solid #FF7A00;
+		height: 15px;
+		width: 160px;
+		text-align: center;
+		margin-left: 145px
+	}
+	.retake{
+		padding: 5px 22px;
+		position: absolute;
+		bottom: 389px;
+		display: block;
+		background-color: orange;
+		border: none;
+		color: white;
+		font-weight: 700;
+	}
 	</style>
+</head>
+<body onload="configure()">
+
+	<!-- CSS -->
+
 
 	<!-- -->
 <div class="wrapper">
+	<input type=button value="Save Snapshot" onClick="saveSnap()">
 	<div id="results">
 		
 	</div>	
-	<input type=button value="Configure" onClick="configure()" class="retake">
-	<label><img src="../php/icons/camera.svg"><input style="display: none;" type=button value="Take Snapshot" onClick="take_snapshot()"></label>
-	<input type=button value="Save Snapshot" onClick="saveSnap()">
+	<input type=button value="Opnieuw" onClick="configure()" class="retake">
+	<label><img style="margin-top: 100px;" src="../php/icons/camera.svg"><input style="display: none;" type=button value="Take Snapshot" onClick="take_snapshot()"></label>
+	
 	<div class="fileChoice">
-            <a href="addPost.php" style="font-weight: 700;color: #FF7A00;">Galery</a>
-            <a href="addPostCam.php">Camera</a>
-        </div>
+            <a href="addPost.php">Galery</a>
+            <a href="addPostCam.php" style="font-weight: 700;color: #FF7A00;">Camera</a>
+    </div>
+	<div class="activeChoice"></div>
+
   
 </div>
 	
